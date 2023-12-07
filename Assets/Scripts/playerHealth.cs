c02799b80fdbfb9f56340f67b2def4901991ad9c
ChaseB62 <chaseb62@educbe.ca>
2023-12-07 10:17:31 -0700

    made things epic

--- Assets/Scripts/playerHealth.cs
+++ Assets/Scripts/playerHealth.cs
@@ -18,6 +18,9 @@ public class playerHealth : MonoBehaviour
 
     public float deathFlingForce = 1000f;
     public float deathRotateForce = 100f;
+
+    public particleHandler particleHandler;
+    public GoToPlayer goToPlayer;
     
 
     public void Update()
@@ -25,6 +28,8 @@ public class playerHealth : MonoBehaviour
         healthSlider.value = health;
         if(health <= 0){
             Die();
+        } else if(health > 0){
+            Revive();
         }
     }
 
@@ -35,17 +40,23 @@ public class playerHealth : MonoBehaviour
 
     public void Die()
     {
+        
         rb.freezeRotation = false;
         if(isDead == false)
         {
+            goToPlayer.Zoom = 5f;
+            goToPlayer.lerpSpeed = 10f;
             rb.AddForce(Random.insideUnitCircle * Random.Range(deathFlingForce, deathFlingForce * 2), ForceMode2D.Impulse);
             rb.AddTorque(Random.Range((deathRotateForce * -1), deathRotateForce), ForceMode2D.Impulse);
+            particleHandler.Die();
         }
         isDead = true;
     }
 
     public void Revive(){
         isDead = false;
+        goToPlayer.Zoom = 10f;
+        goToPlayer.lerpSpeed = 10f;
         transform.rotation = Quaternion.Euler(0, 0, 0);
         rb.freezeRotation = true;
     }
--- Assets/particleHandler.cs
+++ Assets/particleHandler.cs
@@ -8,7 +8,13 @@ public class particleHandler : MonoBehaviour
 
     public GameObject jumpParticle;
 
+    public GameObject deathParticle;
+
     public void Jump(){
         Instantiate(jumpParticle, player.transform.position, player.transform.rotation);
+    }
+
+    public void Die(){
+        Instantiate(deathParticle, player.transform.position, player.transform.rotation);
     }
 }
