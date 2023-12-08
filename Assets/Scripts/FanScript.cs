81049ec4878d12b8159a6b72deeee7243e11f0f1
Destroyer10153 <Aidenmoss07@gmail.com>
2023-12-07 23:34:09 -0700

    Finished jump pad and fan (chas epneis)
    lololol i fiished them

---
+++ Assets/Scripts/FanScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FanScript : MonoBehaviour
+{
+    public float continuousForce = 5f; // Adjust the force as needed
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
+
+            if (playerMovement != null)
+            {
+                // Apply continuous upward force to the player when they stay in the trigger
+                playerMovement.rb.AddForce(Vector2.up * continuousForce, ForceMode2D.Force);
+            }
+        }
+    }
+}
