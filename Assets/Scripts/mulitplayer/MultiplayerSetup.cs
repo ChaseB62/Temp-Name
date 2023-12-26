3fc4d87b3c0206e732c821088b3f47e68a67877d
ChaseB62 <chaseb62@educbe.ca>
2023-12-26 15:09:48 -0700

    doing this so i can revert

--- Assets/Scripts/mulitplayer/MultiplayerSetup.cs
+++ Assets/Scripts/mulitplayer/MultiplayerSetup.cs
@@ -8,7 +8,7 @@ public class MultiplayerSetup : MonoBehaviour
 {
     public PlayerMovement playerMovement;
     public GameObject camera;
-    public GunController gunController;
+    //public GunController gunController;
 
 
     public string nickname;
@@ -22,7 +22,7 @@ public class MultiplayerSetup : MonoBehaviour
     public void IsLocalPlayer()
     {
         playerMovement.enabled = true;
-        gunController.enabled = true;
+        //gunController.enabled = true;
         camera.SetActive(true);
         SetObjectsActive(objectsToEnable, true);
         SetObjectsActive(objectsToDisable, false);
